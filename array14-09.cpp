d20bb614b37c7c60e1650c589d24e0e34aa0c506
aamir-09 <144371539+aamir-09@users.noreply.github.com>
2023-09-14 16:22:18 +0100

    Add files via upload

---
+++ array14-09.cpp
@@ -0,0 +1,15 @@
+// simple array program
+
+#include<iostream>
+using namespace std;
+main()
+{ int a[6];
+  cout<<"enter the value of array";
+  for(int i=0;i<6;i++)
+  {cin>>a[i];
+  }
+  cout<<"\n the array value's are ";
+  for(int i=0;i<6;i++)
+  {cout<<a[i]<<ends;
+  }
+}
